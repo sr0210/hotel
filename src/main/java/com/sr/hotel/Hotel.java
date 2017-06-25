9a143f3feaae4996af76cac2419432a12db47eb8
shirui <shirui_0210@163.com>
2017-06-25 19:48:12 +0800

    add Test file

---
+++ src/main/java/com/sr/hotel/Hotel.java
@@ -0,0 +1,12 @@
+package com.sr.hotel;
+
+/**
+ * Created by shirui on 2017/6/25.
+ */
+public class Hotel {
+
+    public static void main(String[] args){
+        System.out.println("test git");
+    }
+
+}
